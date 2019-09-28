3932c5725fd330d42e4b8520e7b52ccc6a5d8ddf
Mrs Qu <862792055@qq.com>
2019-09-28 09:34:09 +0800

    sdfsdf

--- yb_tuangou/pages/register/index.js
+++ yb_tuangou/pages/register/index.js
@@ -14,8 +14,68 @@ Page({
         value: 'B级'
       },
     ],
-    yqmStatus: false
+    yqmStatus: false,
+    tel: '',
+    pwd: '',
+    repwd: '',
+    yqm: ''
   },
+  register() {
+    const {
+      tel,
+      pwd,
+      repwd,
+      yqm,
+      yqmStatus
+    } = this.data;
+    if (!tel.trim()) {
+      wx.showToast({
+        title: '请输入手机号',
+        icon: 'none'
+      });
+      return
+    } else if (!/^[1][3,4,5,7,8][0-9]{9}$/.test(tel)) {
+      wx.showToast({
+        title: '请输入正确的手机号',
+        icon: 'none'
+      });
+      return
+    } else if (!pwd.trim()) {
+      wx.showToast({
+        title: '请输入密码',
+        icon: 'none'
+      });
+      return
+    } else if (pwd.trim().length < 6) {
+      wx.showToast({
+        title: '密码不能少于6位',
+        icon: 'none'
+      });
+      return
+    } else if (!repwd.trim()) {
+      wx.showToast({
+        title: '请再次输入密码',
+        icon: 'none'
+      });
+      return
+    } else if (repwd.trim() != pwd.trim()) {
+      wx.showToast({
+        title: '两次输入密码不一致',
+        icon: 'none'
+      });
+      return
+    }
+    if (yqmStatus) {
+      if (!yqm.trim()) {
+        wx.showToast({
+          title: '请输入邀请码',
+          icon: 'none'
+        });
+        return
+      }
+    }
+  },
+
   radioChange: function(e) {
     const level = e.detail.value;
     if (level == 'b') {
@@ -27,5 +87,25 @@ Page({
         yqmStatus: false
       })
     }
-  }
+  },
+  bindTel(e) {
+    this.setData({
+      tel: e.detail.value
+    });
+  },
+  bindPwd(e) {
+    this.setData({
+      pwd: e.detail.value
+    });
+  },
+  bindRepwd(e) {
+    this.setData({
+      repwd: e.detail.value
+    });
+  },
+  bindYqm(e) {
+    this.setData({
+      yqm: e.detail.value
+    });
+  },
 });
