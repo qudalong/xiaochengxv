88bdbe52d7bc7b7c7955a75d3e54f5424c2d38f6
Mrs Qu <862792055@qq.com>
2019-10-18 10:32:47 +0800

    报价 排名 接口

--- yb_tuangou/pages/monthCharts/monthCharts.js
+++ yb_tuangou/pages/monthCharts/monthCharts.js
@@ -2,79 +2,110 @@ var months = []
 for (let i = 1; i <= 12; i++) {
   months.push(i)
 }
+import {
+  request
+} from '../../utils/request.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list: [1, 1, 1, 1],
-    month:'',
+    list: [],
+    month: '',
     months: months,
-    showPicker:false,
-    monthF:1
+    showPicker: false,
+    monthF: 1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {},
+  
+  // 查询
+  bindSearch() {
+    var month = this.data.month;
+    if (!month) {
+      wx.showToast({
+        title: '请选择查询时间',
+        icon: 'none'
+      });
+      return
+    }
+    wx.showLoading({
+      title: '查询中...',
+    });
+    this.getDataForMonth();
   },
-  bindSure(){
+
+  // 获取报表数据
+  getDataForMonth() {
+    request({
+      url: 'https://mp.ucloudant.com/app/index.php?i=56&t=0&v=9.2&from=wxapp&c=entry&a=wxapp&do=Dishes&m=zh_dianc&sign=819fcd817f0aeb118075924d12978351&id=5&dishes_type=2',
+    }).then(res => {
+      wx.hideLoading();
+      this.setData({
+        list: res.data[0].goods
+      })
+    });
+  },
+
+  bindSure() {
     this.setData({
       showPicker: false,
       month: this.data.monthF
     });
   },
-  hidePicker(){
+
+  hidePicker() {
     this.setData({
-      showPicker:false
+      showPicker: false
     });
   },
-  showPicker(){
+  showPicker() {
     this.setData({
-      showPicker:true
+      showPicker: true
     });
   },
-  bindChange: function (e) {
+  bindChange: function(e) {
     this.setData({
-      monthF: e.detail.value[0]+1
+      monthF: e.detail.value[0] + 1
     })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
     // wx.showLoading({
     //   title: '刷新中...'
     // })
@@ -83,14 +114,14 @@ Page({
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- yb_tuangou/pages/policy/index.js
+++ yb_tuangou/pages/policy/index.js
@@ -1,10 +1,25 @@
-
+import {
+  request
+} from '../../utils/request.js'
 Page({
-    data: {
-      list:[1,1,1,1,1,1,1,1,1]
-    },
-    onLoad: function(e) {},
-    getinfo: function() {},
-    onShow: function() {},
-    onPullDownRefresh: function() {}
+  data: {
+    rankList: []
+  },
+  onLoad: function(e) {
+    wx.showLoading({
+      title: '查询中...',
+    });
+    this.getRanking();
+  },
+  // 获取排行列表
+  getRanking() {
+    request({
+      url: 'https://mp.ucloudant.com/app/index.php?i=56&t=0&v=9.2&from=wxapp&c=entry&a=wxapp&do=Dishes&m=zh_dianc&sign=819fcd817f0aeb118075924d12978351&id=5&dishes_type=2',
+    }).then(res => {
+      wx.hideLoading();
+      this.setData({
+        rankList: res.data[0].goods
+      })
+    });
+  }
 });
