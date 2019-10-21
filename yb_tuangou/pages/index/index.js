dfdeeef44bf335feee9556913170a12ad95489ec
Mrs Qu <862792055@qq.com>
2019-10-21 09:34:10 +0800

    规则

--- yb_tuangou/pages/index/index.js
+++ yb_tuangou/pages/index/index.js
@@ -30,14 +30,13 @@ Page({
       news: {
         news: '暂无公告'
       }
-
-
-    }
+    },
+    list: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   },
   onLoad: function(e) {
     let user = getApp().getCache("userinfo");
     user || wx.redirectTo({
-        url: "/yb_tuangou/pages/login/index"
+      url: "/yb_tuangou/pages/login/index"
     });
     this.updUser(user.id);
     this.loadNotice();
@@ -54,12 +53,12 @@ Page({
   },
 
   onShow: function() {
-    
+
     var url = this.data.icon.fm;
     this.setData({
       adUrl: url
     });
-    
+
   },
   getAdInfo: function() {
     var n = this;
@@ -71,96 +70,70 @@ Page({
     });
   },
 
- 
- //加载通知
-    loadNotice:function(){
-      var _this = this;
-      o.get("wx/user/selnotice.html", {
-        id: 1
-      }, function (o) {
-        if (o.code == 1) {
 
-          _this.setData({
-            news: o.data.v_content
-          });
+  //加载通知
+  loadNotice: function() {
+    var _this = this;
+    o.get("wx/user/selnotice.html", {
+      id: 1
+    }, function(o) {
+      if (o.code == 1) {
 
-        }
+        _this.setData({
+          news: o.data.v_content
+        });
+
+      }
+    });
+  },
+  updUser(uid) {
+
+    o.post("wx/user/updwx_user.html", {
+      id: uid
+    }, function(o) {
+      if (o.code == 1) {
+        e.setCache("userinfo", o.data);
+
+      }
+    });
+
+
+  },
+  onShow: function() {
+    var url = this.data.icon.fm;
+    this.setData({
+      adUrl: url
+    });
+    getApp().getCache("userinfo") ? '' : wx.redirectTo({
+      url: "/yb_tuangou/pages/login/index"
+    });
+  },
+  getAdInfo: function() {
+    var n = this;
+    o.get("Arliki/get_apply", {}, function(e) {
+      0 == e.code && e.info.ad_pic && n.setData({
+        adUrl: e.info.ad_pic,
+        adShow: !0
       });
-    },
-    updUser(uid){
-
-      o.post("wx/user/updwx_user.html", {
-        id: uid
-      }, function (o) {
-        if (o.code == 1) {
-          e.setCache("userinfo",o.data);      
-
+    });
+  },
+  scancode: function(e) {
+    var t = o.pdata(e).i;
+    if (t == 1) {
+      this.to_order();
+    }
+    wx.scanCode({
+      onlyFromCamera: !0,
+      success: function(e) {
+        if (e.result.indexOf("_") < 0) o.alert("二维码无效");
+        else {
+          var n = e.result.split("_");
+          o.jump("/yb_tuangou/pages/order_page/index?order_no=" + n[0] + "&type=" + n[1] + "&i=" + t);
         }
-      });
-
-
-    },
-    onShow: function() {
-       var url = this.data.icon.fm;
-        this.setData({
-          adUrl: url
-        });
-        getApp().getCache("userinfo") ? '': wx.redirectTo({
-            url: "/yb_tuangou/pages/login/index"
-        });
-    },
-    getAdInfo: function() {
-        var n = this;
-        o.get("Arliki/get_apply", {}, function(e) {
-            0 == e.code && e.info.ad_pic && n.setData({
-                adUrl: e.info.ad_pic,
-                adShow: !0
-            });
-        });
-    },
-    scancode: function(e) {
-        var t = o.pdata(e).i;
-        if(t==1){
-          this.to_order();
-        }
-        wx.scanCode({
-            onlyFromCamera: !0,
-            success: function(e) {
-                if (e.result.indexOf("_") < 0) o.alert("二维码无效"); else {
-                    var n = e.result.split("_");
-                    o.jump("/yb_tuangou/pages/order_page/index?order_no=" + n[0] + "&type=" + n[1] + "&i=" + t);
-                }
-            },
-            fail: function(e) {}
-        });
-    },
-    to_order: function() {
-        o.jump("/yb_tuangou/pages/order/index", 3);
-    },
-    to_order_s: function() {
-        o.jump("/yb_tuangou/pages/order/index?status=1", 3);
-    },
-    to_order_t: function() {
-        o.jump("/yb_tuangou/pages/order/index?status=3", 3);
-    },
-    getinfo: function() {
-        var n = this;
-        o.get("commander/CommanderList", {
-            id: getApp().getCache("userinfo").id
-        }, function(e) {
-            0 == e.code ? n.setData({
-                info: e.info,
-                text: e.info.news.news
-            }) : o.alert(e.msg);
-        });
-    },
-    onShareAppMessage: function() {},
-    onPullDownRefresh: function() {
-        //this.getinfo(),
-         wx.stopPullDownRefresh();
-    },
-
-     
+      },
+      fail: function(e) {}
+    });
+  },
   to_order: function() {
     o.jump("/yb_tuangou/pages/order/index", 3);
   },
@@ -182,7 +155,34 @@ Page({
     });
   },
   onShareAppMessage: function() {},
- 
+  onPullDownRefresh: function() {
+    //this.getinfo(),
+    wx.stopPullDownRefresh();
+  },
+
+
+  to_order: function() {
+    o.jump("/yb_tuangou/pages/order/index", 3);
+  },
+  to_order_s: function() {
+    o.jump("/yb_tuangou/pages/order/index?status=1", 3);
+  },
+  to_order_t: function() {
+    o.jump("/yb_tuangou/pages/order/index?status=3", 3);
+  },
+  getinfo: function() {
+    var n = this;
+    o.get("commander/CommanderList", {
+      id: getApp().getCache("userinfo").id
+    }, function(e) {
+      0 == e.code ? n.setData({
+        info: e.info,
+        text: e.info.news.news
+      }) : o.alert(e.msg);
+    });
+  },
+  onShareAppMessage: function() {},
+
   shenhe() {
     wx.navigateTo({
       url: '/yb_tuangou/pages/shenheList/shenheList'
